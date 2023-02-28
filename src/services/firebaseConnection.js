1c86b083dd3b1b638329ba180ae4f9ccaf991dea
biel.lil <78098857+biellil@users.noreply.github.com>
2023-02-28 19:40:05 -0300

    ok

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -11,12 +11,14 @@ let firebaseConfig = {
   storageBucket: "sistemadechamados-ca58a.appspot.com",
   messagingSenderId: "1089657084671",
   appId: "1:1089657084671:web:58b88b1d2115019ab90aca",
-  measurementId: "G-KNZBKV3ZDL",
+  
 };
 
 
 if(!firebase.apps.length){
   firebase.initializeApp(firebaseConfig);
+
 }
 
 export default firebase;
+
